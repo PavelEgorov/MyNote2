9ce06a1dd41577b93f5dcc8dec96ba1ba7bf9187
PavelEgorov <jonnycomein@gmail.com>
2020-05-06 20:11:41 +0300

    Урок 2.

--- app/src/main/java/com/example/mynote2/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/mynote2/ui/home/HomeFragment.kt
@@ -1,31 +1,54 @@
 package com.example.mynote2.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.mynote2.R
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetBehavior.BottomSheetCallback
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.bottom_sheet_layout.*
+import kotlinx.android.synthetic.main.fragment_home.*
+
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
+            ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
         return root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btn_login.setOnClickListener {
+            Snackbar.make(it, "You are login", Snackbar.LENGTH_LONG)
+                .setAction("Good!!", {
+                    Log.d("SNAKEBAR", "Click")
+                })
+                .show()
+        }
+
+        // init the bottom sheet behavior
+        val bottomSheetBehavior =
+            BottomSheetBehavior.from(bottom_sheet)
+
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+        bottomSheetBehavior.peekHeight = 50
+        bottomSheetBehavior.isHideable = true
     }
 }
