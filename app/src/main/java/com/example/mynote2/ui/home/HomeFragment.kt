ab0aedf85be3fb92d3e42f04257551c7d939e974
PavelEgorov <jonnycomein@gmail.com>
2020-05-11 12:05:07 +0300

    Урок 2.

--- app/src/main/java/com/example/mynote2/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/mynote2/ui/home/HomeFragment.kt
@@ -5,16 +5,13 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.example.mynote2.R
 import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.bottomsheet.BottomSheetBehavior.BottomSheetCallback
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.bottom_sheet_layout.*
 import kotlinx.android.synthetic.main.fragment_home.*
-
 
 class HomeFragment : Fragment() {
 
